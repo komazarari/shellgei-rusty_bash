fdd4124ae72bcaea4a47a40cb3c8f52478074550
Takuto Komazaki <komazarari@gmail.com>
2024-01-08 02:36:22 +0900

    [#3]: implement exit status

--- src/core.rs
+++ src/core.rs
@@ -1,16 +1,46 @@
 //SPDX-FileCopyrightText: 2022 Ryuichi Ueda ryuichiueda@gmail.com
 //SPDX-License-Identifier: BSD-3-Clause
 
+use nix::sys::wait;
+use nix::sys::wait::WaitStatus;
+use nix::unistd::Pid;
+use std::collections::HashMap;
+
 pub struct ShellCore {
     pub history: Vec<String>,
+    pub vars: HashMap<String, String>,
 }
 
 impl ShellCore {
     pub fn new() -> ShellCore {
-        let conf = ShellCore{
+        let mut core = ShellCore{
             history: Vec::new(),
+            vars: HashMap::new(),
         };
 
-        conf
+        core.vars.insert("?".to_string(), "0".to_string());
+        core
+    }
+
+    pub fn wait_process(&mut self, child: Pid) {
+        let exit_status = match wait::waitpid(child, None) {
+            Ok(WaitStatus::Exited(_pid, status)) => {
+                status
+            },
+            Ok(WaitStatus::Signaled(pid, signal, _coredump)) => {
+                eprintln!("{}: Terminated by signal {}", pid, signal);
+                128 + signal as i32
+            },
+            Ok(unsuppoted) => {
+                eprintln!("Unsupported wait status: {:?}", unsuppoted);
+                1
+            },
+            Err(err) => {
+                panic!("Failed to wait: {}", err);
+            }
+        };
+
+        eprintln!("Exit status: {}", exit_status);
+        self.vars.insert("?".to_string(), exit_status.to_string());
     }
 }
--- src/elem_command.rs
+++ src/elem_command.rs
@@ -5,39 +5,69 @@ use std::process;
 use crate::{ShellCore,Feeder};
 use nix::unistd;
 use nix::unistd::ForkResult;
-use nix::sys::wait;
+use nix::errno::Errno;
 use std::ffi::CString;
 use std::env;
 use std::path::Path;
 
 pub struct Command {
-    text: String,
+    _text: String,
     args: Vec<String>,
     cargs: Vec<CString>,
 }
 
 impl Command {
-    pub fn exec(&mut self, _core: &mut ShellCore) { //引数_coreはまだ使いません
-        if self.text == "exit\n" {
-            process::exit(0);
+    pub fn exec(&mut self, core: &mut ShellCore) { //引数_coreはまだ使いません
+        if self.args[0] == "exit" {
+            eprintln!("exit!");
+            if self.args.len() > 1 {
+                core.vars.insert("?".to_string(), self.args[1].clone());
+            }
+
+            let exit_status = match core.vars["?"].parse::<i32>() {
+                Ok(n) => n%256,
+                Err(_) => {
+                    eprintln!("sh: exit: {}: numeric argument required", core.vars["?"]);
+                    2
+                }
+            };
+            process::exit(exit_status);
         }
 
         if self.args[0] == "cd" && self.args.len() > 1 {
             let path = Path::new(&self.args[1]);
-            if env::set_current_dir(&path).is_err() {
+            let exit_status = match env::set_current_dir(&path) {
+                Ok(_) => 0,
+                Err(_) => 1,
+            };
+            if exit_status != 0 {
                 println!("Failed to change directory.");
             }
+
+            core.vars.insert("?".to_string(), exit_status.to_string());
             return;
         }
 
         match unsafe{unistd::fork()} {
             Ok(ForkResult::Child) => {
-                let err = unistd::execvp(&self.cargs[0], &self.cargs);
-                println!("Failed to exec: {:?}", err);
-                process::exit(127);
+                match unistd::execvp(&self.cargs[0], &self.cargs) {
+                    Err(Errno::EACCES) => {
+                        println!("sh: {}: Permission denied.", &self.args[0]);
+                        process::exit(126);
+                    },
+                    Err(Errno::ENOENT) => {
+                        println!("sh: {}: command not found.", &self.args[0]);
+                        process::exit(127);
+                    },
+                    Err(err) => {
+                        println!("Failed to exec: {:?}", err);
+                        process::exit(127);
+                    },
+                    _ => ()
+                }
             },
             Ok(ForkResult::Parent { child }) => {
-                let _ = wait::waitpid(child, None);
+                core.wait_process(child);
             },
             Err(err) => panic!("Failed to fork. {}", err),
         }
@@ -58,7 +88,7 @@ impl Command {
             .collect();
 
         if args.len() > 0 {
-            Some( Command {text: line, args, cargs} )
+            Some( Command {_text: line, args, cargs} )
         } else {
             None
         }
